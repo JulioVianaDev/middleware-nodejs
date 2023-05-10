fa42f6d347ef6d10d2c40e96ac7c9b6df40deadf
juliovianadev <juliovianadev@gmail.com>
2023-05-10 14:57:28 -0300

    rootDir

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,9 @@
 const path= require('path')
 const router =require('express').Router();
-
+const rootDir = require('../util/path')
 router.get('/',(req, res, next) => {
     console.log('In another middleware!');
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 module.exports = router;
