2da91f9feac0187b1d46c5346c05113b1098290e
juliovianadev <juliovianadev@gmail.com>
2023-05-10 14:47:52 -0300

    serving html files

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,9 @@
+const path= require('path')
 const router =require('express').Router();
 
 router.get('/',(req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    res.sendFile(path.join(__dirname,'../','views','shop.html'));
 });
 
 module.exports = router;
